76b14ab196319f075b399a199e3a84d53090d00b
KH54 <pkh543@naver.com>
2021-10-06 16:27:09 +0900

    2021/10/06

--- src/프로그래머스/문자열내마음대로정렬하기.java
+++ src/프로그래머스/문자열내마음대로정렬하기.java
@@ -1,17 +1,40 @@
 package 프로그래머스;
 
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class 문자열내마음대로정렬하기 {
 
     public static void main(String[] args) {
-       String[] strings = {"sun", "bed", "car"};
-       int n = 1;
-       
-       solution(strings,n);
-//       String[] strings = {"abce","abcd","cdx"};
+        String[] strings = { "sun", "bed", "car" };
+        int n = 1;
+
+        System.out.println(solution(strings, n));
     }
+
     public static String[] solution(String[] strings, int n) {
-        
-        String[] answer = null;
-        return answer;
+
+//           List<String> list = new ArrayList<>();
+//                   
+//           for(int i = 0 ; i < strings.length; i++) {
+//               list.add(strings[i].charAt(n) + strings[i]);
+//           }
+//           
+//           Collections.sort(list);
+//           String[] answer = new String[list.size()];
+//           
+//           for(int i = 0 ; i < list.size(); i++) {
+//               answer[i] = list.get(i).substring(1, list.get(i).length());
+//           }
+//           return answer;
+
+        Arrays.sort(strings);
+        Arrays.sort(strings, Comparator.comparing((s) -> s.substring(n, n + 1)));
+
+        return strings;
     }
+
 }
