f51b7d8d55e09f2677a26cc6f31a8441112ea0cd
KH54 <pkh543@naver.com>
2021-10-13 17:47:19 +0900

    10.13 알고리즘

---
+++ src/com/programmers/level1/없는숫자더하기.java
@@ -0,0 +1,19 @@
+package com.programmers.level1;
+
+public class 없는숫자더하기 {
+    public static void main(String[] args) {
+        int[] numbers = { 1, 2, 3, 4, 6, 7, 8, 0 };
+        
+        System.out.println(solution(numbers));
+    }
+
+    public static int solution(int[] numbers) {
+        int answer = 45;
+        
+        for(int number : numbers) {
+            answer -= number;
+        }
+        
+        return answer;
+    }
+}
