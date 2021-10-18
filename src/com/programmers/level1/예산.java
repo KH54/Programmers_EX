c468e8f9c79842bde1f1c9a86fc306a7f63fa318
KH54 <pkh543@naver.com>
2021-10-18 11:11:18 +0900

    10.15

---
+++ src/com/programmers/level1/예산.java
@@ -0,0 +1,30 @@
+package com.programmers.level1;
+
+import java.util.Arrays;
+
+public class ¿¹»ê {
+
+    public static void main(String[] args) {
+        int[] d = { 2, 2, 3, 3 };
+        int budget = 10;
+
+        System.out.println(solution(d, budget));
+    }
+
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if (budget < 0)
+                break;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
