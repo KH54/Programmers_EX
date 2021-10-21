139e39c29207a7de8f235e18806419f9d0e1df0a
KH54 <pkh543@naver.com>
2021-10-21 17:20:34 +0900

    10.21

--- src/com/programmers/level1/다트게임.java
+++ src/com/programmers/level1/다트게임.java
@@ -1,26 +1,80 @@
 package com.programmers.level1;
 
+import java.util.regex.Pattern;
+
 public class 다트게임 {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
+        String dartResult = "10D10T*10S";
 
-	}
+        System.out.println(solution(dartResult));
+    }
 
-	public int solution(String dartResult) {
-		int answer = 0;
+    public static int solution(String dartResult) {
+        int answer = 0;
 
-		String[] point = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-		String[] sdt = { "s", "d", "t" };
-		String[] bonus = { "*", "#", "x" };
+        String[] tmp = dartResult.split("");
+        int cac = 0;
+        int count = 0;
+        int[] sum = new int[3];
 
-		String[] tmp = dartResult.split("");
+        for (int i = 0; i < dartResult.length(); i++) {
+            if (Pattern.matches("^[0-9]*$", tmp[i])) {
 
-		for (int i = 0; i < dartResult.length(); i++) {
-			if (tmp[i].equals(point[i]) || tmp[i].equals(sdt[i]) || tmp[i].equals(bonus[i])) {
-				
-			}
-		}
-		
-		return answer;
-	}
+                if (tmp[i + 1].equals("0")) {
+                    cac = 10;
+                    i++;
+                } else {
+                    cac = Integer.parseInt(tmp[i]);
+                }
+            } else if (Pattern.matches("^[A-Z]*$", tmp[i])) {
+                switch (tmp[i]) {
+                case "S":
+                    sum[count] = cac;
+                    count++;
+                    break;
+                case "D":
+                    cac = (int) Math.pow(cac, 2);
+                    sum[count] = cac;
+                    count++;
+
+                    break;
+                case "T":
+                    cac = (int) Math.pow(cac, 3);
+                    sum[count] = cac;
+                    count++;
+                    break;
+                }
+            } else if (Pattern.matches("^[*,#]$", tmp[i])) {
+                switch (tmp[i]) {
+                case "*":
+                    if (i == 2) {
+                        sum[0] = sum[0] * 2;
+                    } else if (i == 4 || i == 5) {
+                        sum[0] = sum[0] * 2;
+                        sum[1] = sum[1] * 2;
+                    } else {
+                        sum[1] = sum[1] * 2;
+                        sum[2] = sum[2] * 2;
+                    }
+                    break;
+                case "#":
+                    if (i == 2) {
+                        sum[0] = sum[0] * (-1);
+                    } else if (i == 4 || i == 5) {
+                        sum[1] = sum[1] * (-1);
+                    } else {
+                        sum[2] = sum[2] * (-1);
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        for (int cc : sum) {
+            answer += cc;
+        }
+        return answer;
+    }
 }
