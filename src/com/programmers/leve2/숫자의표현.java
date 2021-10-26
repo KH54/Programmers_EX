2a8681a5e4d8c81eb99eb473ed0050a9e8970df6
KH54 <pkh543@naver.com>
2021-10-26 23:05:02 +0900

    10.26

---
+++ src/com/programmers/leve2/숫자의표현.java
@@ -0,0 +1,26 @@
+package com.programmers.leve2;
+
+public class 숫자의표현 {
+    public static void main(String[] args) {
+        int n = 15;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int answer = 1;
+        
+        for(int i = 1; i < n/2+1 ; i++) {
+            int sum = 0;
+            int plus = i;
+            
+            while(sum<=n) {
+                sum+=plus++;
+            }
+            if(sum==n) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
