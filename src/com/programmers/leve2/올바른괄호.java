c73c72cd2316d81f9a1411b1e363144c4200f82b
KH54 <pkh543@naver.com>
2021-11-18 13:26:36 +0900

    11.18

---
+++ src/com/programmers/leve2/올바른괄호.java
@@ -0,0 +1,47 @@
+package com.programmers.leve2;
+
+public class ¿Ã¹Ù¸¥°ýÈ£ {
+    public static void main(String[] args) {
+        String s = ")()(";
+
+        System.out.println(solution(s));
+    }
+
+    static boolean solution(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            
+            count += s.charAt(i) == '(' ? 1 : -1;
+
+            if (count < 0) {
+                return false;
+            }
+        }
+
+        return count == 0 ? true : false;
+    }
+}
+
+//    static boolean solution(String s) {
+//        boolean answer = true;
+//
+//        Stack<Character> stack = new Stack<>();
+//        char a = ' ';
+//        
+//        for (int i = 0; i < s.length(); i++) {
+//            a = s.charAt(i);
+//            
+//            if (a == '(') {
+//                stack.push(a);
+//            } else {
+//                if (!stack.empty()) {
+//                    stack.pop();
+//                } else {
+//                    return false;
+//                }
+//            }
+//        }
+//        answer = stack.empty() ? true : false;
+//        return answer;
+//    }
+//}
