80212c45cc6a48c8be11f0569b08fe52263a3f9c
KH54 <pkh543@naver.com>
2021-11-22 23:56:27 +0900

    11.22

---
+++ src/com/programmers/leve2/n제곱가장큰수.java
@@ -0,0 +1,25 @@
+package com.programmers.leve2;
+
+import java.util.Arrays;
+
+public class n제곱가장큰수 {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int left = 2;
+        int right = 5;
+
+        System.out.println(Arrays.toString(solution(n, left, right)));
+    }
+
+    public static int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int) (right-left)+1];
+        int count =0;
+        
+        for(long i = left; i <= right; i++) {
+            answer[count++] = (int) (Math.max(i/n, i%n)+1);
+        }
+
+        return answer;
+    }
+}
