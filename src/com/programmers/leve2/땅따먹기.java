b9a215d1975e6386eaae1a72bf112ffe0f4923b3
KH54 <pkh543@naver.com>
2021-12-06 12:56:52 +0900

    12.06

--- src/com/programmers/leve2/땅따먹기.java
+++ src/com/programmers/leve2/땅따먹기.java
@@ -1,34 +1,31 @@
 package com.programmers.leve2;
 
-public class ¶¥µû¸Ô±â {
+import java.util.Arrays;
+
+public class ë•…ë”°ë¨¹ê¸° {
 
     public static void main(String[] args) {
-        int[][] land = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 }, { 5, 100, 100, 100 } };
+        int[][] land = { 
+                { 1, 2, 3, 5 }, 
+                { 5, 6, 7, 8 }, 
+                { 4, 3, 2, 1 }};
         System.out.println(solution(land));
     }
 
     static int solution(int[][] land) {
-        int answer = 0;
-        int max = 0;
-        int no = 0;
-        int nno = 0;
-
-        for (int i = 0; i < land.length; i++) {
-            max = 0;
-            for (int j = 0; j < 4; j++) {
-
-                if (j == nno) {
-                    continue;
-                }
-                if (max < land[i][j]) {
-                    max = land[i][j];
-                    no = j;
-                }
-            }
-            nno = no;
-            answer += max;
+        
+        
+        for(int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+            land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+            land[i][2] += Math.max(Math.max(land[i-1][1], land[i-1][0]), land[i-1][3]);
+            land[i][3] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][0]);
         }
+        
+        int[] answer =  land[land.length-1];
+        Arrays.sort(answer);
+        
 
-        return answer;
+        return answer[3];
     }
 }
